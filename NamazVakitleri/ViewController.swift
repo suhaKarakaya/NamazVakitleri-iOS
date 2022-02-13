f1ba4faa180cb03c98b2269d15d344ffe2e37beb
suhaKarakaya <suhabskarakaya@gmail.com>
2022-02-13 23:42:49 +0300

    second  commit

---
+++ NamazVakitleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamazVakitleri
+//
+//  Created by Süha Karakaya on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
